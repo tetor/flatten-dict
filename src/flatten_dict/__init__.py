1eeeb8ca5f20338a08d6d00439fa8028e7fc870f
Takashi Etou <tetor.takashi.etou@gmail.com>
2019-12-13 15:24:37 +0900

    fix: PyInstaller error

--- src/flatten_dict/__init__.py
+++ src/flatten_dict/__init__.py
@@ -3,4 +3,4 @@ from .flatten_dict import flatten, unflatten  # noqa: F401
 
 
 __all__ = ['flatten_dict', 'reducer', 'splitter']
-__version__ = pkg_resources.get_distribution("flatten-dict").version
+__version__ = "0.2.0"
